96b777dc4bc3e778b78752665a22325ad269a59c
Araxielfenix <araxielfenix@outlook.com>
2025-05-29 22:14:02 -0600

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,11 @@
 import { CronJob } from 'cron';
-
+import express from 'express';
 import https from 'https';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(PORT, () => console.log(`Server is live on port ${PORT}`));
 
 // get env RENDER_EXTERNAL_URL
 const render_url = process.env.RENDER_EXTERNAL_URL
